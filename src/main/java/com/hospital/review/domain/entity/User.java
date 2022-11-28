a449017e5da5babae5b6d7931dc4fb9465f65a8a
SuInWoo <wbo1026@naver.com>
2022-11-28 13:47:33 +0900

    User userName 컬럼 unique 처리

--- src/main/java/com/hospital/review/domain/entity/User.java
+++ src/main/java/com/hospital/review/domain/entity/User.java
@@ -5,10 +5,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @AllArgsConstructor
 @Getter
@@ -19,7 +16,10 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(unique = true)
     private String userName;
+
     private String password;
     private String emailAddress;
 }
